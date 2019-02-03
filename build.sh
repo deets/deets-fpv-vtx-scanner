3180ae6884bae05c0ec706ce0f2ea2ebf88ef5f7
Diez B. Roggisch <deets@web.de>
2019-02-03 15:04:43 +0100

    Adapted *.sh to work on mac and linux

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 export PATH=~/esp/xtensa-esp32-elf/bin:$PATH
-export IDF_PATH=/home/deets/software/vc/esp-idf
-make -j28
+export IDF_PATH=~/software/vc/esp-idf
+
+if [ `uname` == 'Darwin' ]; then
+    CORES=4
+else
+    CORES=28
+fi
+
+make -j$CORES
--- flash.sh
+++ flash.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 export PATH=~/esp/xtensa-esp32-elf/bin:$PATH
-export IDF_PATH=/home/deets/software/vc/esp-idf
-make -j28 flash
+export IDF_PATH=~/software/vc/esp-idf
+
+BAUD=460800
+
+if [ `uname` == 'Darwin' ]; then
+    CORES=4
+    PORT=/dev/tty.SLAB_USBtoUART
+else
+    PORT=/dev/ttyUSB0
+    CORES=28
+fi
+
+
+make ESPBAUD=$BAUD ESPPORT=$PORT -j$CORES flash
