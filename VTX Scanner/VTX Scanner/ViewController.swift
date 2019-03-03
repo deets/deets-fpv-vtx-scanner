781e48fe3c413f5bc9d9e4a9e81f3ca8fbebbb5c
Diez B. Roggisch <deets@web.de>
2019-03-03 14:45:00 +0100

    Add iOS app

---
+++ VTX Scanner/VTX Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VTX Scanner
+//
+//  Created by Diez B. Roggisch on 02.03.19.
+//  Copyright © 2019 Diez B. Roggisch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
