af9884de2d11cf4700c6ebaded80bb5f4731b295
Diez B. Roggisch <deets@web.de>
2019-03-20 21:22:55 +0100

    Add random data view

--- VTX Scanner/VTX Scanner/LaptimerView.swift
+++ VTX Scanner/VTX Scanner/LaptimerView.swift
@@ -12,6 +12,28 @@ class LaptimerView: UIView {
     private var displayLink : CADisplayLink?
     private var color = UIColor(named: "black")
     
+    private let maxRSSI = CGFloat(2100.0)
+    private var points: [CGFloat] = []
+    
+    var duration = 1.0 {
+        didSet {
+            if duration < 1.0 {
+                duration = 1.0
+            }
+            initializePointArray()
+        }
+    }
+    
+    private func initializePointArray() {
+        //let numExtraBufferingPoints = pointsPerSecond() * extraBufferingDuration
+        let numPoints = Int(Double(bounds.width) + 1) // + numExtraBufferingPoints)
+        if points.count != numPoints {
+            points = Array(repeating: 0, count: numPoints)
+//            endTime = nil
+//            lastWritePosition = nil
+        }
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initialize()
@@ -36,13 +58,14 @@ class LaptimerView: UIView {
                 })
             );
         } )
+        initializePointArray()
     }
     
     private func run() {
         displayLink = CADisplayLink(target: self,
-                                        selector: #selector(step))
+                                    selector: #selector(step))
         displayLink!.add(to: .current,
-                        forMode: .common)
+                         forMode: .common)
         color = UIColor.red
         setNeedsDisplay()
     }
@@ -55,14 +78,20 @@ class LaptimerView: UIView {
     }
     
     @objc private func step() {
-    
+        setNeedsDisplay()
     }
 
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         color?.setStroke()
-        path.move(to: CGPoint(x: 0.0, y: bounds.height))
-        path.addLine(to: CGPoint(x: bounds.width, y: 0))
+        let step = bounds.width / CGFloat(points.count - 1)
+        let hf =  bounds.height / maxRSSI;
+        path.move(to: CGPoint(x: 0.0, y: hf * points[0]))
+        for x in 1..<points.count {
+            //let v = points[x]
+            let v = CGFloat.random(in: 0...maxRSSI)
+            path.addLine(to: CGPoint(x: CGFloat(x) * step, y: hf * v))
+        }
         path.stroke()
     }
 }
