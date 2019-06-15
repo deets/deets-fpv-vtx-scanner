2b58c3ad1ddf82dd90597acaa51ea09683b903b9
Diez B. Roggisch <deets@web.de>
2019-06-15 14:01:05 +0200

    ios stuff

--- VTX Scanner/Dependencies/Package.swift
+++ VTX Scanner/Dependencies/Package.swift
@@ -8,8 +8,9 @@ let package = Package(
         .library(name: "Dependencies", type: .dynamic, targets: ["Dependencies"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "3.0.0"),
-      .package(url: "https://github.com/nst/BinUtils.git", from: "0.1.0"),
+      .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0"),
+      .package(url: "https://github.com/deets/ReactiveCocoa.git", .revision("e6b0d920a80ffe6c4d69cfbadc8fa145377be0d3")),
+      .package(url: "https://github.com/deets/BinUtils.git", .revision("c2c8e9777bc47d1d86e6b60fec79855e5b67c056")),
       .package(url: "https://github.com/deets/SwiftSVG.git", .exact("2.1.7")),
     ],
     targets: [
@@ -17,6 +18,7 @@ let package = Package(
         name: "Dependencies",
         dependencies: [
           "ReactiveSwift",
+          "ReactiveCocoa",
           "BinUtils",
           "SwiftSVG",
         ],
