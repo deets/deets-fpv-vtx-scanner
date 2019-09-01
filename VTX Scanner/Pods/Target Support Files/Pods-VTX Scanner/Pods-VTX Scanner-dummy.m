ebb318765e6413ed34f023a977f448e6b34bb24c
Diez B. Roggisch <deets@web.de>
2019-09-01 20:16:59 +0200

    Migrated to cocoa pods

---
+++ VTX Scanner/Pods/Target Support Files/Pods-VTX Scanner/Pods-VTX Scanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VTX_Scanner : NSObject
+@end
+@implementation PodsDummy_Pods_VTX_Scanner
+@end
--- VTX Scanner/VTX Scanner/Bluetooth/BTVTXScannerDelegate.swift
+++ VTX Scanner/VTX Scanner/Bluetooth/BTVTXScannerDelegate.swift
@@ -1,6 +1,5 @@
 import Foundation
 import CoreBluetooth
-import BinUtils
 import ReactiveSwift
 import Result
 
