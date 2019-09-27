437ed2a9922689e1eaa2c4bf4cacd5d74cf32bc6
Diez B. Roggisch <deets@web.de>
2019-09-27 17:18:21 +0200

    Generalised storage and restoring, works for peak length now, too

--- main/storage.cpp
+++ main/storage.cpp
@@ -1,10 +1,43 @@
 #include "storage.hh"
 
 #include <esp_log.h>
+#include <functional>
+#include <sstream>
 
 #define DEETS_FVP_SCANNER_NAMESPACE "de-fpv-scan"
 
 #define K_SELECTED_CHANNEL "sel-chan"
+
+namespace {
+std::string hash(const char* arg)
+{
+  const auto h = std::hash<std::string>{}(arg);
+  std::stringstream s;
+  s << h;
+  return s.str();
+}
+
+} // end ns anonymous
+
+#define CHECK_AND_STORE(attributename) \
+  if(_app_state.attributename != _persisted_app_state.attributename) \
+  { \
+    ESP_LOGE("storage", "Storing "#attributename); \
+    err = nvs_set_u32(_nvs_handle, hash(#attributename).c_str(), _app_state.attributename); \
+    commit &= (err == ESP_OK || err == ESP_ERR_NVS_NOT_FOUND); \
+  } \
+
+
+#define RESTORE(attributename) \
+  err = nvs_get_u32(_nvs_handle, hash(#attributename).c_str(), (uint32_t*)&_app_state.attributename); \
+  switch (err) { \
+  case ESP_OK: \
+    break; \
+  case ESP_ERR_NVS_NOT_FOUND: \
+    break; \
+  default : \
+    ESP_LOGE("storage", "Error (%s) reading!", esp_err_to_name(err)); \
+  }
 
 Storage::Storage(app_state_t& app_state)
   : _app_state(app_state)
@@ -30,17 +63,9 @@ Storage::Storage(app_state_t& app_state)
 void Storage::store()
 {
   esp_err_t err;
-  bool commit = false;
-  if(_app_state.selected_channel != _persisted_app_state.selected_channel)
-  {
-    ESP_LOGE("storage", "Storing selected_channel");
-    err = nvs_set_u32(_nvs_handle, K_SELECTED_CHANNEL, _app_state.selected_channel);
-    commit = (err == ESP_OK || err == ESP_ERR_NVS_NOT_FOUND);
-    if(!commit)
-    {
-      ESP_LOGE("storage", "Failed storing selected_channel");
-    }
-  }
+  bool commit = true;
+  CHECK_AND_STORE(selected_channel);
+  CHECK_AND_STORE(peak_detection_config.peak_size);
   if(commit)
   {
     err = nvs_commit(_nvs_handle);
@@ -53,15 +78,7 @@ void Storage::restore()
 {
   ESP_LOGD("storage", "Storage::restore");
 
-  // we can ignore the error because
-  // it won't touch the passed field.
-  auto err = nvs_get_u32(_nvs_handle, K_SELECTED_CHANNEL, &_app_state.selected_channel);
-  switch (err) {
-  case ESP_OK:
-    break;
-  case ESP_ERR_NVS_NOT_FOUND:
-    break;
-  default :
-    ESP_LOGE("storage", "Error (%s) reading!", esp_err_to_name(err));
-  }
+  esp_err_t err;
+  RESTORE(selected_channel);
+  RESTORE(peak_detection_config.peak_size);
 }
