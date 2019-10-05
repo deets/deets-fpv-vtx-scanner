b85afea3124f010c89a5fd35c027f3ac0f217c30
Diez B. Roggisch <deets@web.de>
2019-10-05 15:16:38 +0200

    Add macOS-app while waiting for SwiftUI to become available

---
+++ VTXScannerLaptimer/VTXScannerLaptimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VTXScannerLaptimer
+//
+//  Created by Diez B. Roggisch on 05.10.19.
+//  Copyright © 2019 Diez B. Roggisch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VTXScannerLaptimer/VTXScannerLaptimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VTXScannerLaptimer
+//
+//  Created by Diez B. Roggisch on 05.10.19.
+//  Copyright © 2019 Diez B. Roggisch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
