897017c1ad140aaa5d2c80033fe38ea589c892ae
Diez B. Roggisch <deets@web.de>
2019-10-06 17:29:32 +0200

    Add table view that is model driven to star playing around with listed vtx gizmos

--- VTXScannerLaptimer/VTXScannerLaptimer/ViewController.swift
+++ VTXScannerLaptimer/VTXScannerLaptimer/ViewController.swift
@@ -10,9 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+  
+    struct TestEntry {
+        let name: String
+        var status: String
+    }
+    
+    let testData: [TestEntry] = [
+        TestEntry(name: "Anne", status: "Ok"),
+        TestEntry(name: "Diez", status: "Tired")
+       ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -25,3 +39,52 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDataSource {
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return testData.count;
+  }
+
+}
+
+extension ViewController: NSTableViewDelegate {
+
+  fileprivate enum CellIdentifiers {
+    static let NameCell = NSUserInterfaceItemIdentifier(rawValue: "NameCellID")
+    static let StatusCell = NSUserInterfaceItemIdentifier(rawValue: "StatusCellID")
+    
+  }
+    
+   
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+
+    var text: String = ""
+    var cellIdentifier: NSUserInterfaceItemIdentifier?
+    if(row >= testData.count)
+    {
+        return nil
+    }
+    let item = testData[row]
+
+    // 2
+    if tableColumn == tableView.tableColumns[0] {
+      text = item.name
+      cellIdentifier = CellIdentifiers.NameCell
+    } else if tableColumn == tableView.tableColumns[1] {
+        text = item.status
+        cellIdentifier = CellIdentifiers.StatusCell
+    }
+    
+    // 3
+    if let cell = tableView.makeView(withIdentifier: cellIdentifier!, owner: nil) as? NSTextField {
+        cell.stringValue = text
+      return cell
+    } else {
+        let cell = NSTextField.init()
+        cell.stringValue = text
+        cell.identifier = cellIdentifier
+        return cell
+    }
+  }
+
+}
