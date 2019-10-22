aa7738b44ccd49e883af48ce9abf197f0da69a6e
dir <dir@ableton.com>
2019-10-22 18:44:14 +0200

    Add capturing of rssi values

--- python-data-logger/LaptimerView.py
+++ python-data-logger/LaptimerView.py
@@ -1,14 +1,50 @@
+import datetime
+import pathlib
+import threading
+import queue
+import csv
+
 from Cocoa import (
     NSView,
     NSBezierPath,
     NSColor,
     )
 
-from objc import IBOutlet
+from objc import IBOutlet, IBAction, python_method
+
+class Capture:
+
+    BASE = pathlib.Path("/tmp")
+
+    def __init__(self):
+        fname = self.BASE / "vtx-scanner-log-{}.csv".format(datetime.datetime.now().isoformat().replace(":", "-"))
+        self._f = fname.open("w")
+        self._q = queue.Queue()
+        self._t = threading.Thread(target=self._run)
+        self._t.start()
+
+    def stop(self):
+        self._q.put(None)
+        self._t.join()
+
+    def feed(self, samples):
+        self._q.put(samples)
+
+    @python_method
+    def _run(self):
+        while True:
+            data = self._q.get()
+            if data is None:
+                break
+            for value in data:
+                self._f.write(f"{value}\n")
+        self._f.close()
+
 
 class LaptimerView(NSView):
 
-    FILTER_GAIN = 0.01
+    FILTER_GAIN = 0.005
+    RETAINED_SAMPLES = 2000
 
     rssiValuesPerSecond = IBOutlet("rssiValuesPerSecond")
 
@@ -17,6 +53,7 @@ class LaptimerView(NSView):
         self.graphColor = NSColor.whiteColor()
         self._last_ts = None
         self._rssi_values = []
+        self._capture = None
         self._filtered_value_per_second = 1000.0
 
         return super(LaptimerView, self).initWithFrame_(frame)
@@ -45,5 +82,17 @@ class LaptimerView(NSView):
             self.rssiValuesPerSecond.setIntValue_(int(self._filtered_value_per_second))
         self._last_ts = ts
         self._rssi_values.extend(values)
-        self._rssi_values[:-1000] = []
+        self._rssi_values[:-self.RETAINED_SAMPLES] = []
         self.setNeedsDisplay_(True)
+        if self._capture:
+            self._capture.feed(values)
+
+    @IBAction
+    def captureLog_(self, sender):
+        if self._capture is None:
+            self._capture = Capture()
+            sender.setTitle_("Stop")
+        else:
+            self._capture.stop()
+            self._capture = None
+            sender.setTitle_("Capture Log")
